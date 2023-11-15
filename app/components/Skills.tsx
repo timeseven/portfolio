235f4b4dbb44c3475491fc5618331afa123de584
Even Qian <evenwin88@gmail.com>
2023-11-15 22:25:51 +1100

    udpates

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -5,9 +5,9 @@ export default function Skills() {
         <h3 className="text-4xl text-center font-extrabold text-gray-900 dark:text-gray-100">Skill Set👓</h3>
         <ul className="max-w-3xl mx-auto p-4 text-xl text-left leading-7 text-gray-800 dark:text-gray-100">
           <li key="lan" className="mt-2 lg:flex lg:justify-between lg:gap-x-4">
-            <span className="w-full lg:w-1/4">Basics: </span>
+            <span className="w-full lg:w-1/4">Language: </span>
             <span className="w-full lg:w-3/4">
-              <strong>JavaScript, TypeScript, HTML5, CSS3</strong>
+              <strong>JavaScript, TypeScript,</strong> Python, HTML5, CSS3
             </span>
           </li>
           <li key="fe" className="mt-2 lg:flex lg:justify-between lg:gap-x-4">
