5b91d92d67977a37886240765eb37e58eca23e4b
Even Qian <evenwin88@gmail.com>
2024-01-08 11:12:50 +1100

    updates

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -7,31 +7,31 @@ export default function Skills() {
           <li key="lan" className="mt-2 lg:flex lg:justify-between lg:gap-x-4">
             <span className="w-full lg:w-1/4">Language: </span>
             <span className="w-full lg:w-3/4">
-              <strong>JavaScript, TypeScript,</strong> Python, HTML5, CSS3
+              <strong>JavaScript, TypeScript, Python, </strong>HTML5, CSS3
             </span>
           </li>
           <li key="fe" className="mt-2 lg:flex lg:justify-between lg:gap-x-4">
             <span className="w-full lg:w-1/4">Front End: </span>
             <span className="w-full lg:w-3/4">
-              <strong>React, Redux, Next.js, Tailwind CSS,</strong> Headless UI, Shadcn UI, React Native, Bootstrap
+              <strong>React, Redux, Next.js, Tailwind CSS,</strong> Bootstrap
             </span>
           </li>
           <li key="be" className="mt-2 lg:flex lg:justify-between lg:gap-x-4">
             <span className="w-full lg:w-1/4">Back End: </span>
             <span className="w-full lg:w-3/4">
-              <strong>Node.js, Express,</strong> Restful API, Prisma, NestJS, GraphQL, TanStack
+              <strong>Node.js, Django, Restful API, Express,</strong> NestJS, Prisma, GraphQL
             </span>
           </li>
           <li key="db" className="mt-2 lg:flex lg:justify-between lg:gap-x-4">
             <span className="w-full lg:w-1/4">DB: </span>
             <span className="w-full lg:w-3/4">
-              <strong>SQL, MySQL, MongoDB,</strong> PostgreSQL
+              <strong>MySQL, PostgreSQL, MongoDB</strong>
             </span>
           </li>
           <li key="tools" className="mt-2 lg:flex lg:justify-between lg:gap-x-4">
             <span className="w-full lg:w-1/4">Tools: </span>
             <span className="w-full lg:w-3/4">
-              <strong>AWS, Jenkins, Docker,</strong> Git, Webpack, Jest, Cypress
+              <strong>AWS, Jenkins, Docker, Git,</strong> Github, Webpack, Jest, Cypress
             </span>
           </li>
         </ul>
